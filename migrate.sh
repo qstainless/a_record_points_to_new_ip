15d0b479e975917af7b60268889cbac5124a84f3
Queue Stainless <16870263+qstainless@users.noreply.github.com>
2022-12-15 16:05:40 -0500

    Update script to take arguments

--- migrate.sh
+++ migrate.sh
@@ -5,7 +5,11 @@
 #
 # This is simple and useful script to monitor a domain's modified A record
 # that points to a new hosting provider, instead of manually pinging
-# the domain until propagation is complete. 
+# the domain until propagation is complete.
+#
+# The script takes two arguments:
+# target_ip - the IP address of the subdomain/domain in the target hosting provider to which the A record will point.
+# domain_to_check - the subdomain/domain in the target hosting provider
 #
 # The script can also be modified to write the update messages to a text file
 # ```
@@ -18,27 +22,33 @@
 # echo "Still pointing to $ip @ $(date)" >> migration_check.txt
 # ```
 
+# check if proper arguments are passed
+if [ $# -eq 0 ]; then
+  echo "No target ip address or domain provided."
+  echo "usage: sh migrate.sh <target_ip_address> <target_domain_to_check>"
+  exit 1
+fi
+
 clear
 
-# Replace xxx.xxx.xxx.xxx below with the IP to which your A record will point.
-# target_ip is the IP address of your subdomain/domain in the target hosting provider.
-# Keep the colon (:) at the end of the target_ip
-target_ip="xxx.xxx.xxx.xxx:"
-domain_to_check="subdomain.domain.com"
+target_ip="$1:"
+domain_to_check=$2
+
+# interval to check in minutes
 interval=5
 
 echo "Checking for migration to $target_ip at $interval-minute intervals.\n"
 
 while true; do
-    ip=$(ping -c 1 $domain_to_check | grep "64 bytes from"| awk '{print $4}')
+  ip=$(ping -c 1 $domain_to_check | grep "64 bytes from" | awk '{print $4}')
 
-    if [ "$ip" = "$target_ip" ]; then
-        echo "Migration complete @ $(date)";
-        break;
-    else
-        echo "Still pointing to $ip @ $(date)"
-    fi
+  if [ "$ip" = "$target_ip" ]; then
+    echo "Migration complete @ $(date)"
+    break
+  else
+    echo "Still pointing to $ip @ $(date)"
+  fi
 
-    # Wait the interval value in minutes
-    sleep $((60*interval))
+  # Wait the interval value in minutes
+  sleep $((60 * interval))
 done
